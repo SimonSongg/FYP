5058ff3d71613d0bd05ba08b2e2d104d75ca2027
Tianchen Song <sf995511sf@qq.com>
2022-01-22 19:20:37 +0800

    Update chassis_control.py

--- fyp/scripts/chassis_control.py
+++ fyp/scripts/chassis_control.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-from cv2 import MOTION_TRANSLATION
 import rospy
 import serial
 import os
@@ -8,9 +7,13 @@ from geometry_msgs.msg import Vector3
 
 os.system('echo %s | sudo -S %s' % ('20001007', 'chmod 777 /dev/ttyTHS0'))
 pub = rospy.Publisher('motor', Vector3, queue_size=5)
+ser = serial.Serial("/dev/ttyTHS0", 57600, timeout=2)
+
+motorL = 0.0
+motorR = 0.0
 
 
-def callback(data):
+def callbackSpeed(data):
     global motorL
     global motorR
     motorL = data.x
@@ -19,12 +22,16 @@ def callback(data):
 
 
 def motorControl(speedL, speedR):
-    ser = serial.Serial("/dev/ttyTHS0", 57600, timeout=2)
 
-    print(ser)
-    while(1):
-        message = "#Baffff040,000,000,000"
-        msg_encode = message.encode('ascii')
-        print(msg_encode.hex())
-        ser.write(msg_encode)
-        print("1")
+    #print(ser)
+    #while(1):
+    message = "#Baffff040,000,000,000"
+    msg_encode = message.encode('ascii')
+    print(msg_encode.hex())
+    ser.write(msg_encode)
+    print("1")
+
+
+if __name__ == "__main__":
+    rospy.Subscriber('speed_info', Vector3, callbackSpeed)
+    rospy.Timer(rospy.Duration(0.1), motorControl, False)
