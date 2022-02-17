39f9a4076f6b28b0b943096dd62f9398293599ea
SimonSongg <sf995511sf@qq.com>
2022-02-18 01:23:02 +0800

    Add a self_defined message type Add a publisher that publishes UWB info

--- fyp/scripts/chassis_control.py
+++ fyp/scripts/chassis_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 import serial
@@ -10,7 +10,7 @@ os.system('echo %s | sudo -S %s' % ('20001007', 'chmod 777 /dev/ttyTHS0'))
 pub = rospy.Publisher('motor', Vector3, queue_size=5)
 ser = serial.Serial("/dev/ttyTHS0", 57600, timeout=2)
 
-motorL = 0.0
+motorL = 10.0
 motorR = 0.0
 
 
@@ -22,16 +22,16 @@ def callbackSpeed(data):
     # rospy.loginfo
 
 
-def motorControl(speedL, speedR):
+def motorControl(event):
     
 
     #print(ser)
-    while(1):
-        message="#Ba%s%s%s%s%03d,%03d,%03d,%03d" % (('r' if speedL >= 0 else 'f'), 
-          ('r' if speedL >= 0 else 'f'), 
-          ('r' if speedR >= 0 else 'f'), 
-          ('r' if speedR >= 0 else 'f'), 
-          abs(speedL),abs(speedL),abs(speedR), abs(speedR))
+    
+        message="#Ba%s%s%s%s%03d,%03d,%03d,%03d" % (('r' if motorL >= 0 else 'f'), 
+          ('r' if motorL >= 0 else 'f'), 
+          ('r' if motorR >= 0 else 'f'), 
+          ('r' if motorR >= 0 else 'f'), 
+          abs(motorL),abs(motorL),abs(motorR), abs(motorR))
         msg_encode = message.encode('ascii')
         print(msg_encode.hex())
         ser.write(msg_encode)
@@ -39,5 +39,7 @@ def motorControl(speedL, speedR):
         
         
 if __name__ == "__main__":
+     rospy.init_node('chassis_motion')
      rospy.Subscriber('speed_info', Vector3, callbackSpeed)
-     rospy.Timer(rospy.Duration(0.1), motorControl(motorL,motorR), False)
+     rospy.Timer(rospy.Duration(0.1), motorControl, False)
+     rospy.spin()
---
+++ fyp/scripts/uwb.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import rospy
+import serial
+import os
+from fyp.msg import Uwb_info
+#from geometry_msgs.msg import Vector3
+
+uwb_data = Uwb_info()
+os.system('echo %s | sudo -S %s' % ('20001007', 'chmod 777 /dev/ttyUSB0'))
+pub = rospy.Publisher('uwbArray', Uwb_info, queue_size = 5)
+rospy.init_node('UWBPublisher', anonymous = False)
+
+ser = serial.Serial("/dev/ttyUSB0",115200,timeout=2)
+
+while not rospy.is_shutdown():
+    line = ser.readline()
+    line = line.decode()
+    if line.startswith('mc'):
+        print(line)
+        uwb_data.uwb_info[0] = float(line[3:5])
+        uwb_data.uwb_info[1] = int(line[6:14],16) / 1000
+        uwb_data.uwb_info[2] = int(line[15:23],16) / 1000
+        uwb_data.uwb_info[3] = int(line[24:32],16) / 1000
+        uwb_data.uwb_info[4] = int(line[33:41],16) / 1000
+        print(uwb_data.uwb_info)
+        pub.publish(uwb_data)
+
+
+
+
+
+
+    
