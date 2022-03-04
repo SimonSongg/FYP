4b6aca970f20095c2592fc0b7025744a90fac0ef
SimonSongg <sf995511sf@qq.com>
2022-03-05 00:12:27 +0800

    Change a little in UWB localization. Find a stupid bug caused by bad configuration of the postion of the anchors.

--- fyp/scripts/uwb.py
+++ fyp/scripts/uwb.py
@@ -4,11 +4,12 @@ import rospy
 import serial
 import os
 from fyp.msg import Uwb_info
+from std_msgs.msg import Int64MultiArray
 #from geometry_msgs.msg import Vector3
-
+data_array = [0,0,0,0,0]
 uwb_data = Uwb_info()
 os.system('echo %s | sudo -S %s' % ('20001007', 'chmod 777 /dev/ttyUSB0'))
-pub = rospy.Publisher('uwbArray', Uwb_info, queue_size = 5)
+pub = rospy.Publisher('uwbArray', Int64MultiArray, queue_size = 5)
 rospy.init_node('UWBPublisher', anonymous = False)
 
 ser = serial.Serial("/dev/ttyUSB0",115200,timeout=2)
@@ -18,13 +19,14 @@ while not rospy.is_shutdown():
     line = line.decode()
     if line.startswith('mc'):
         print(line)
-        uwb_data.uwb_info[0] = int(line[3:5])
-        uwb_data.uwb_info[1] = int(line[6:14],16) 
-        uwb_data.uwb_info[2] = int(line[15:23],16) 
-        uwb_data.uwb_info[3] = int(line[24:32],16) 
-        uwb_data.uwb_info[4] = int(line[33:41],16) 
-        print(uwb_data.uwb_info)
-        pub.publish(uwb_data)
+        data_array[0] = int(line[3:5])
+        data_array[1] = int(line[6:14],16) 
+        data_array[2] = int(line[15:23],16) 
+        data_array[3] = int(line[24:32],16) 
+        data_array[4] = int(line[33:41],16) 
+        print(data_array)
+        publishdata = Int64MultiArray(data=data_array)
+        pub.publish(publishdata)
 
 
 
--- fyp/scripts/uwb_localization_ros.py
+++ fyp/scripts/uwb_localization_ros.py
@@ -3,7 +3,9 @@
 import rospy
 import os
 from geometry_msgs.msg import Vector3
+from std_msgs.msg import Int64MultiArray
 from fyp.msg import Uwb_info
+
 
 from ctypes import *
 
@@ -29,6 +31,8 @@ location = UWBMsg()
 anchorArray=(UWBMsg*3)()
 
 distanceArray=(c_int*3)()
+#distanceArray=[0,0,0]
+
 
 anchorArray[0].x=0
 
@@ -38,17 +42,17 @@ anchorArray[0].z=2
 
 
 
-anchorArray[1].x=0
+anchorArray[1].x=0.87
 
-anchorArray[1].y=10
+anchorArray[1].y=0
 
 anchorArray[1].z=2
 
 
 
-anchorArray[2].x=10
+anchorArray[2].x=0
 
-anchorArray[2].y=10
+anchorArray[2].y=1.97
 
 anchorArray[2].z=2
 
@@ -58,14 +62,12 @@ position_data = Vector3()
 
 #distanceArray = [ctypes.c_int(7100),ctypes.c_int(7100),ctypes.c_int(7100)]
 
-def callbackPosition(data):
+def callbackPosition(msgg):
     global distanceArray
-    #print(distanceArray)
-    #print(distanceArray[1])
-    print(data.uwb_info[1])
-    distanceArray[0] = data.uwb_info[1]
-    distanceArray[1] = 7100
-    distanceArray[2] = 7100
+
+    distanceArray[0] = msgg.data[1]
+    distanceArray[1] = msgg.data[2]
+    distanceArray[2] = msgg.data[3]
 
 def calculatePosition(event):
     global distanceArray
@@ -86,6 +88,6 @@ def calculatePosition(event):
 
 if __name__ == "__main__":
      rospy.init_node('position_publisher', anonymous = False)
-     rospy.Subscriber('uwbArray', Uwb_info, callbackPosition)
+     rospy.Subscriber('uwbArray', Int64MultiArray, callbackPosition)
      rospy.Timer(rospy.Duration(0.1), calculatePosition, False)
      rospy.spin()
